859ae22fd7a1de9ffc0389188757fb2bda0b56d6
Sevan Babaxanloo <sevan_babaxanloo@yahoo.com>
2024-01-19 04:13:55 -0800

    changes

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../Assets/home-image/icon-logo.svg";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const styles = {
   headerContainer:
